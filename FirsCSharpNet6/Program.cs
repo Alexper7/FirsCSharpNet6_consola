e3cc1fe2998a3a59d0c552601dc94f237d589f16
Alex Perales <alex2020per@gmail.com>
2025-05-08 20:33:59 -0500

    Add project files.

---
+++ FirsCSharpNet6/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string curso = "Desarrollo de aplicaciones web 2025-01";
+int edad = 20;
+double salario = 1200;
+var alumno = "Alex Perales";
+var peso = 75;
+
+
---
+++ FirsCSharpNet6/persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirsCSharpNet6
+{
+    public class persona
+    {
+        public int dni { get; set; }
+        public string nombreCompleto { get; set; }
+        public string telefono { get; set; }
+        public bool estado { get; set; }
+        public persona()
+        {
+        }
+        public persona(int dni, string nombreCompleto, string telefono, bool estado)
+        {
+            this.dni = dni;
+            this.nombreCompleto = nombreCompleto;
+            this.telefono = telefono;
+            this.estado = estado;
+        }
+
+    }
+}
