28ebcba8663ba4372eb4c6d0c120aae54471ebf3
Alex Perales <alex2020per@gmail.com>
2025-05-08 23:15:50 -0500

    Nuevo comit

--- FirsCSharpNet6/Program.cs
+++ FirsCSharpNet6/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+using FirsCSharpNet6;
+
 Console.WriteLine("Hello, World!");
 
 string curso = "Desarrollo de aplicaciones web 2025-01";
@@ -7,4 +10,26 @@ double salario = 1200;
 var alumno = "Alex Perales";
 var peso = 75;
 
+var persona1 = new Persona(){ dni = 12345678, nombreCompleto = "Juan Perez", telefono = "987654321",estado = true}
+;
+var persona2 = new Persona(){ dni = 12345678, nombreCompleto = "Maria Lopez",telefono = "987654321",estado = true};
+var persona3 = new Persona(44009988, "Pedro Garcia", "987654321", true);
+
+var personas = new List<Persona>();
+personas.Add(persona1);
+personas.Add(persona2);
+personas.Add(persona3); 
+foreach(var item in personas)
+{
+    Console.WriteLine(item.dni + " | " + item.nombreCompleto);
+}
+var busqueda = personas.Find(x => x.dni == 44009988);
+if (busqueda != null)
+{
+    Console.WriteLine("Dni encontrado: " + busqueda.dni);
+}
+else
+{
+    Console.WriteLine("El dni no existe en la lista");
+}
 
--- FirsCSharpNet6/persona.cs
+++ FirsCSharpNet6/persona.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace FirsCSharpNet6
 {
-    public class persona
+    public class Persona
     {
         public int dni { get; set; }
         public string nombreCompleto { get; set; }
         public string telefono { get; set; }
         public bool estado { get; set; }
-        public persona()
+        public Persona()
         {
         }
-        public persona(int dni, string nombreCompleto, string telefono, bool estado)
+        public Persona(int dni, string nombreCompleto, string telefono, bool estado)
         {
             this.dni = dni;
             this.nombreCompleto = nombreCompleto;
